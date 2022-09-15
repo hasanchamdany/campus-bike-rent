a4b03b62a38bf46da74578ace884156761fada28
hasanchamdany <hasanchamdany@gmail.com>
2022-09-15 22:57:09 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+//import routes
+const bikeRoutes = require(`./routes/bike`)
+
+//Routes 
+app.get('/', (req, res) => {
+    res.send('Routes Test');
+});
+
+//connection to DB
+mongoose.connect(process.env.DB_CONNECTION)
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Database connection error'))
+db.once('open', () => {
+    console.log("Database is connected");
+})
+
+//listen 
+app.listen(process.env.PORT, () => {
+    console.log('running in ${process.env.PORT}');
+})
---
+++ routes/bike.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router()
+
+// router.get('/', (req, res) => {
+//     res.send("Bike Routes test")
+// })
+
+//Create
+
+
+module.exports = router
