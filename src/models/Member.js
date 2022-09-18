101d5aa00e08c6d028ad15600337bf3eb28cd188
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-09-18 15:19:44 +0700

    edit bike and member schema

--- src/models/Member.js
+++ src/models/Member.js
@@ -2,14 +2,26 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const memberSchema = new mongoose.Schema({
+  niu: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
   },
-  NIU: {
+  email: {
+    type: String,
+    required: true,
+  },
+  pass: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
     type: String,
     required: true,
   },
 });
 
-export default mongoose.model('Member', memberSchema);
+export default mongoose.model("Member", memberSchema);
