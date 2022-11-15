0e91adfb8c79944b372c5581868e1601ee4dbd36
rein7703 <reinhart.siregar@mail.ugm.ac.id>
2022-11-15 17:44:36 +0700

    Adding Interface for the Profile Page
    Using tailwind CSS, the UI for Profile including the menu buttons on the left side and the content on the right side (including user info and edit info page) is added

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Navbar from "./../components/Navbar/Navbar.jsx";
+import Navbar from "./../components/Navbar/Navbar.jsx"
+import ProfileCard from "../components/Cards/ProfileCard.jsx"
+//import ProfileContentCard from "../components/Cards/ProfileContentCard.jsx"
+import EditProfileCard from '../components/Cards/EditProfileCard.jsx';
 
 const profile = () => {
   return (
     <>
     <Navbar />
-    <div className="text-center text-white bgdefault w-full h-[910px]">
-      Profile
+
+    <div className="App h-screen flex flex-1 justify-center items-center bgdefault gap-x-4">
+      <ProfileCard />
+      <EditProfileCard />
     </div>
   </>
   )
