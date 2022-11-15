4b5614c9948b5754289b0ed304c09f82a89c396e
raisasalsabily <raisa.salsabil88@gmail.com>
2022-11-15 20:31:28 +0700

    fix(home): move About part to the home page

--- client/src/components/SidePage/SidePageHome.jsx
+++ client/src/components/SidePage/SidePageHome.jsx
@@ -3,38 +3,47 @@ import button from "../../assets/Images/button.svg"
 import riders from "../../assets/Images/riders.svg"
 import viewMore from "../../assets/Images/viewMore.svg"
 
-
 export default function SideCard() {
   return (
     <div className="flex-col">
       <div className="flex justify-center">
-          <div className="block mt-40 ml-40">
+        <div className="block mt-40 ml-40">
           {/*div BISA - button*/}
-            <div className=" pt-16 text-8xl font-bold font-Inter justify-left ml-24">
-              B<span className=" text-orange">i</span>SA
-            <div className="text-2xl">Bike Sharing App</div>  
+          <div className=" pt-16 text-8xl font-bold font-Inter justify-left ml-24">
+            B<span className=" text-orange">i</span>SA
+            <div className="text-2xl">Bike Sharing App</div>
           </div>
           <div id="button rent" className="mt-6 ml-24 h-f14 w-64 my-40">
-              <button>
-                <a href="/profile">
-                <img src={button} alt="rent now" 
-                className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"></img>
-                </a>
-              </button> 
+            <button>
+              <a href="/profile">
+                <img
+                  src={button}
+                  alt="rent now"
+                  className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"
+                ></img>
+              </a>
+            </button>
           </div>
         </div>
-      <div className="flex m-auto">
-        <img src={riders} alt="bike hello" 
-            className="rounded-xl w-9/12 h-9/12 m-auto"></img>
-      </div></div>      
-      <div> 
+        <div className="flex m-auto">
+          <img
+            src={riders}
+            alt="bike hello"
+            className="rounded-xl w-9/12 h-9/12 m-auto"
+          ></img>
+        </div>
+      </div>
+      <div>
         <div className="flex justify-center">
           <button>
-            <img src={viewMore} alt="view more"
-            className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"></img>
+            <img
+              src={viewMore}
+              alt="view more"
+              className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"
+            ></img>
           </button>
         </div>
-      </div> 
-    </div> 
+      </div>
+    </div>
   )
 }
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from "./../components/Navbar/Navbar.jsx";
-import SidePageHome from "../components/SidePage/SidePageHome.jsx"
+import React from "react"
+import Navbar from "./../components/Navbar/Navbar.jsx"
+import About from "./../components/Tem/About.jsx"
+import SidePageHome from "./../components/SidePage/SidePageHome.jsx"
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    <div className="text-center text-white bgdefault w-full h-[910px]">
-    <SidePageHome/>  
-    </div>
-  </>
+      <Navbar />
+      {/* <About className="flex flex-col items-center justify-center w-full" /> */}
+      <div className="text-center text-white bgdefault w-full h-[910px]">
+        <SidePageHome />
+      </div>
+    </>
   )
 }
-  
+
 export default Home
