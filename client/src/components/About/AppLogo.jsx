4fe9a25f706cd5903cfad689328bd024116b85e5
raisasalsabily <raisa.salsabil88@gmail.com>
2022-11-15 20:45:02 +0700

    fix(about): split about page into reusable components and combine it with home page

---
+++ client/src/components/About/AppLogo.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+export default function AppLogo() {
+  return (
+    <>
+      <div id="loginTitle" className="flex">
+        <h1 className="text-7xl font-bold text-white">B</h1>
+        <h1 className="text-7xl font-bold text-yellow-dark">i</h1>
+        <h1 className="text-7xl font-bold text-white">SA</h1>
+      </div>
+      <div>
+        <h4 className="text-md font-semi-bold">Bike Sharing App</h4>
+      </div>
+    </>
+  )
+}
---
+++ client/src/components/Contributors/ContribBox.jsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+import ContribPerson from "../Contributors/ContribPerson"
+
+import anya from "../../assets/Images/Contributors/anya.webp"
+import loid from "../../assets/Images/Contributors/loid.webp"
+import yor from "../../assets/Images/Contributors/yor.webp"
+import sylvia from "../../assets/Images/Contributors/sylvia.webp"
+import damian from "../../assets/Images/Contributors/damian.webp"
+
+export default function ContribBox() {
+  return (
+    <>
+      <h4 className="text-xl">Contributors:</h4>
+      {/* --------- contributor card div start --------- */}
+      <div className="flex flex-row gap-6">
+        <ContribPerson
+          personImg={sylvia}
+          personAlt="Laili"
+          personName="Laili"
+        />
+        <ContribPerson
+          personImg={damian}
+          personAlt="Reinhart"
+          personName="Reinhart"
+        />
+        <ContribPerson personImg={loid} personAlt="Hasan" personName="Hasan" />
+        <ContribPerson personImg={yor} personAlt="Raisa" personName="Raisa" />
+        <ContribPerson personImg={anya} personAlt="Sasa" personName="Sasa" />
+      </div>
+      {/* --------- contributor card div end --------- */}
+    </>
+  )
+}
