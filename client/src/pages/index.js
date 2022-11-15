227ad621cef3f691dde01e9662e670126465dfda
sasanadya <sasanadyarizki2@gmail.com>
2022-11-15 17:54:59 +0700

    feat(home): add interface for the Home Page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from "./../components/Navbar/Navbar.jsx";
+import SidePageHome from "../components/SidePage/SidePageHome.jsx"
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <div className="text-center text-white bgdefault w-full h-[910px]">
-      Home Page
+    <SidePageHome/>  
     </div>
   </>
   )
