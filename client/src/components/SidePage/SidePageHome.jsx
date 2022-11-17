adfe1d13844db862a1cd5887ea30f63653e2fcc8
raisasalsabily <raisa.salsabil88@gmail.com>
2022-11-18 02:42:28 +0700

    fix(home): fix home page code and add responsiveness

--- client/src/components/SidePage/SidePageHome.jsx
+++ client/src/components/SidePage/SidePageHome.jsx
@@ -3,38 +3,53 @@ import button from "../../assets/Images/button.svg"
 import riders from "../../assets/Images/riders.svg"
 import viewMore from "../../assets/Images/viewMore.svg"
 
-
 export default function SideCard() {
   return (
     <div className="flex-col">
-      <div className="flex justify-center">
-          <div className="block mt-40 ml-40">
-          {/*div BISA - button*/}
-            <div className=" pt-16 text-8xl font-bold font-Inter justify-left ml-24">
-              B<span className=" text-orange">i</span>SA
-            <div className="text-2xl">Bike Sharing App</div>  
+      {/* main start */}
+      <div className="flex justify-center content-center pt-24 pb-16">
+        {/* page left start */}
+        <div className="w-full flex flex-col justify-center items-center content-center lg:w-1/3">
+          {/*div BISA - logo*/}
+          <div className="pt-16 text-8xl font-bold font-Inter justify-left lg:ml-24">
+            B<span className=" text-orange">i</span>SA
+            <div className="text-2xl">Bike Sharing App</div>
           </div>
-          <div id="button rent" className="mt-6 ml-24 h-f14 w-64 my-40">
-              <button>
-                <a href="/profile">
-                <img src={button} alt="rent now" 
-                className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"></img>
-                </a>
-              </button> 
+          <div id="button rent" className="mt-6 lg:ml-24 h-f14 w-64 my-40">
+            <button>
+              <a href="/profile">
+                <img
+                  src={button}
+                  alt="rent now"
+                  className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"
+                ></img>
+              </a>
+            </button>
           </div>
         </div>
-      <div className="flex m-auto">
-        <img src={riders} alt="bike hello" 
-            className="rounded-xl w-9/12 h-9/12 m-auto"></img>
-      </div></div>      
-      <div> 
-        <div className="flex justify-center">
+        {/* page left end */}
+        {/* riders start */}
+        <div className="hidden m-auto lg:flex lg:w-2/3">
+          <img
+            src={riders}
+            alt="bike hello"
+            className="rounded-xl w-9/12 h-9/12 m-auto"
+          ></img>
+        </div>
+        {/* riders end */}
+      </div>
+      {/* main end */}
+      <div>
+        <div className="flex justify-center mb-8">
           <button>
-            <img src={viewMore} alt="view more"
-            className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"></img>
+            <img
+              src={viewMore}
+              alt="view more"
+              className="hover:scale-[1.02] ease-in-out rounded-xl w-9/12 h-9/12 m-auto"
+            ></img>
           </button>
         </div>
-      </div> 
-    </div> 
+      </div>
+    </div>
   )
 }
